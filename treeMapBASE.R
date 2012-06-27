6fd695215558d6bcf652dc6cbbe26780d5a7d0ac
naja <najko@gmx.de>
2012-06-27 17:34:07 +0200

    add treemap

---
+++ treeMapBASE.R
@@ -0,0 +1,33 @@
+#plotting treemap
+require(treemap)
+require(countrycode)
+
+#read data
+
+my.data <- read.csv("./data/baseData.csv",header=T,sep=",")
+
+co.doc <- aggregate(as.numeric(Dokumente) ~ Land, data=my.data, sum)
+co.rep <- as.data.frame(table(unlist(my.data$Land)))
+
+data.tree <- merge(co.doc,co.rep,by.x="Land",by.y="Var1")
+
+#massage
+data.tree$Land <- toupper(data.tree$Land)
+data.tree$Land[data.tree$Land == "UK"] <- "GB" ##UK to GB
+
+#region/continent
+region <- countrycode(data.tree$Land, "iso2c", "region")
+continent <- countrycode(data.tree$Land, "iso2c", "continent")
+
+#cbind
+data.tree <- cbind(data.tree,region,continent)
+colnames(data.tree) <- c("Land","Dokumente","Quelle","Region","Kontinent")
+
+data.tree <- na.omit(data.tree)
+write.csv(data.tree, "./data/base_continent_regions.csv")
+
+#plot
+
+pdf("./plot/treemapBase.pdf")
+tmPlot(data.tree, index=c("Region"), vSize="Quelle", vColor="Dokumente", type="dens")
+dev.off()
